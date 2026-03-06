a49a358cfbcd42d717d8ddd46a46b7b591cbf24f
P <petrubraha@gmail.com>
2026-03-06 10:16:38 +0200

    config: gradle for java code generation

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+    val smithyGradleVersion: String by settings
+    plugins {
+        id("software.amazon.smithy.gradle.smithy-jar").version(smithyGradleVersion)
+    }
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "InAndOut-API-Modelling"
